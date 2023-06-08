c835585a97a375b2456023c3f2fb4893c4c9b1aa
beny shouraki <benyaminchor@gmail.com>
2023-06-08 22:52:27 +0300

    add preparing_for_chat_question func

--- finalProExe.py
+++ finalProExe.py
@@ -2,13 +2,19 @@ import argparse
 import json
 
 
+def preparing_for_chat_question(file_path):
+
+    actions = []
+
+    return actions
+
 async def chat_gpt_answer(file_path):
 
     response_dict = {}
 
     return json.dumps(response_dict, indent="\n")
 
-def main():
+async def main():
     user_path = argparse.ArgumentParser(description="Extract text from a PowerPoint file")
     user_path.add_argument("PowerPointFile", type=str, help="Path to the PowerPoint presentation file")
     file = user_path.parse_args()
