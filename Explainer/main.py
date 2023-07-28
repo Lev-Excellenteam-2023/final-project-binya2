01b446d2113b2bb0e44d7cda78027b57978b4e38
beny shouraki <benyaminchor@gmail.com>
2023-07-28 15:25:30 +0300

    Fixing DB file and adjusting main file

--- Explainer/main.py
+++ Explainer/main.py
@@ -1,11 +1,15 @@
 import asyncio
-import os
 import logging
-import time
+import os
+from datetime import datetime
+
+from dotenv import load_dotenv
+from sqlalchemy.orm import sessionmaker
+
 import chatGPT
 import makeJson
 import ppFile
-from dotenv import load_dotenv
+from db.dbManagement import *
 
 load_dotenv()
 
@@ -17,10 +21,10 @@ OUTPUTS_FOLDER = r"C:\exselentim\python\final-project-binya2\file_pro\outputs"
 # Configure the logger
 logging.basicConfig(filename='my_logs.log', filemode='w', level=logging.DEBUG,
                     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-# Create the logger object
 logger = logging.getLogger('my_logger')
 
-async def handling_file(file_path):
+
+async def handling_file(file_path) -> bool:
     """
     This function gets a path to a presentation and returns a dictionary with the slide text and the response from GPT-3.
     """
@@ -34,28 +38,33 @@ async def handling_file(file_path):
 
 async def main_Explainer():
     try:
-        if not os.path.exists(UPLOADS_FOLDER):
-            os.makedirs(UPLOADS_FOLDER)
-        if not os.path.exists(OUTPUTS_FOLDER):
-            os.makedirs(OUTPUTS_FOLDER)
-        files_after_processing = []
-        current_time = time.time()
         while True:
-            files_before_processing = [file for file in os.listdir(UPLOADS_FOLDER) if
-                                       file.endswith(".pptx") and file not in files_after_processing]
-            for file in files_before_processing:
-                logger.info(f"Processing file: {file}  in: {time.ctime(current_time)}")
-                if await handling_file(os.path.join(UPLOADS_FOLDER, file)):
-                    files_after_processing.append(file)
-                    logger.info(f"Finished processing file: {file} in: {time.ctime(current_time)}")
+            Base.metadata.create_all(engine)
+            Session = sessionmaker(bind=engine)
+            session = Session()
+            asyncio.get_event_loop().run_until_complete(main_Explainer())
+            files_before_processing = session.query(Upload).filter_by(status=Status.PENDING).all()
 
+            for file in files_before_processing:
+                file.status = Status.PROCESSING
+                logger.info(f"Processing file: {file}  in: {datetime.now()}")
+                if await handling_file(os.path.join(UPLOADS_FOLDER, file)):
+                    file.status = Status.DONE
+                    logger.info(f"Finished processing file: {file} in: {datetime.now()}")
+                else:
+                    file.status = Status.FAILED
+                    logger.error(f"Error occurred while processing file: {file} in: {datetime.now()}")
+                file.finish_time = datetime.now()
+            session.close()
             await asyncio.sleep(10)
     except Exception as e:
-        print(f"Error occurred while processing file. Error message: {str(e)}")
-    except KeyboardInterrupt:
-        print("Keyboard interrupt occurred")
+        logger.error(f"Error occurred while processing file. Error message: {str(e)}")
         return
 
 
 if __name__ == "__main__":
-    asyncio.get_event_loop().run_until_complete(main_Explainer())  # Run the main as async function
+    if not os.path.exists(UPLOADS_FOLDER):
+        os.makedirs(UPLOADS_FOLDER)
+    if not os.path.exists(OUTPUTS_FOLDER):
+        os.makedirs(OUTPUTS_FOLDER)
+    engine = create_engine('sqlite:///example.db')  # Run the main as async function
